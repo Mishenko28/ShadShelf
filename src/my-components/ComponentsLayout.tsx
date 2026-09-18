bc31959358962e2ea37f46bbd570b991c530b37b
John Thomas Alog <johnthomasalog@gmail.com>
2026-09-18 17:56:31 +0800

    feat: update ComponentsLayout for improved responsiveness and user messaging on small screens

--- src/my-components/ComponentsLayout.tsx
+++ src/my-components/ComponentsLayout.tsx
@@ -3,13 +3,19 @@ import Sidebar from "./Sidebar"
 
 export default function ComponentsLayout() {
 	return (
-		<div className="flex h-svh items-center justify-center py-4">
-			<div className="mx-auto flex h-full w-7xl gap-1.5">
-				<Sidebar />
-				<main className="flex-1 overflow-hidden">
-					<Outlet />
-				</main>
+		<>
+			<div className="flex h-svh flex-col items-center justify-center px-8 text-center text-sm lg:hidden">
+				<span>This page is not available on small screens.</span>
+				<span>Please use screen larger that 1024px.</span>
 			</div>
-		</div>
+			<div className="hidden h-svh items-center justify-center p-4 lg:flex">
+				<div className="mx-auto flex h-full w-7xl gap-1.5">
+					<Sidebar />
+					<main className="flex-1 overflow-hidden">
+						<Outlet />
+					</main>
+				</div>
+			</div>
+		</>
 	)
 }
